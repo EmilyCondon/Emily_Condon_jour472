f9a17e942224c0368aa7d76e4c4309b8f22a8ecf
EmilyCondon <157755880+EmilyCondon@users.noreply.github.com>
2024-02-07 23:34:51 -0500

    HW

--- lab_03/pre_lab_03/Condon_Test.R
+++ lab_03/pre_lab_03/Condon_Test.R
@@ -1,1 +1,11 @@
 #Test file from Emily
+
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("janitor")
+install.packages("rmarkdown")
+install.packages("skimr")
+install.packages("swirl")
+library(swirl)
+ls()
+
