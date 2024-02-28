f1b9cb1a11bfeb7dd88d55cc6918c045a1ca79e5
EmilyCondon <157755880+EmilyCondon@users.noreply.github.com>
2024-02-28 15:56:10 -0500

    HW 2/28

---
+++ Project1.R
@@ -0,0 +1,25 @@
+#Step #2 Data Notebook
+
+```{r}
+#A - Add research questions here
+
+```
+
+```{r}
+#B - Background here
+
+```
+
+```{r}
+#C - Load libraries
+
+library(tidyverse)
+library(rio)
+library(janitor)
+
+```
+
+```{r}
+#D - Import the data
+
+```
